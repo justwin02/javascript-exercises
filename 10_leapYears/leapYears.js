293baf0b9e085804f3760e6e318782a060b4a290
Justin Nguyen <Jnguy129@gmail.com>
2025-09-02 17:02:41 -0500

    Created leapYears that determines wheter a given year is leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    const divideFour = num % 4 === 0;
+    const divideCentury = num % 100 === 0;
+    const divideFourHundred = num % 400 === 0;
 
+    if(divideFour && (!divideCentury || divideFourHundred)){
+        return true;
+    }else 
+        return false;
 };
 
 // Do not edit below this line
