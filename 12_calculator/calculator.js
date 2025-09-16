f293d12540397c83b4576a5e25014798dbaaa953
Justin Nguyen <Jnguy129@gmail.com>
2025-09-16 09:46:41 -0500

    feat: implement calculator functions and pass all tests for add, subtract, sum, multiply, power, and factorial

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if(n < 0){
+    return "ERROR"
+  }else if(n === 0 | n === 1){
+    return 1;
+  }else{
+    let result = 1;
+    for(let i = 2; i <= n; i++){
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
